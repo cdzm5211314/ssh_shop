e91706641bd0547171d7e020821834265d6b2b1b
cdzm5211314 <123309778@qq.com>
2017-08-03 15:38:16 +0800

    用户模块--->跳转到注册页面

---
+++ src/cn/itcast/shop/user/action/UserAction.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cn.itcast.shop.user.action;
+
+/**
+ * @ClassName: UserAction
+ * @Description: TODO 
+ * @Author: ChenD
+ * @CreateDate: Aug 3, 2017 3:11:48 PM
+ */
+public class UserAction {
+
+	/**
+	 * 跳转到注册页面
+	 */
+	public String registPage(){
+		
+		return "registPageSUCCESS";
+	}
+	
+}
